2e2b7e8b7fbff040a79a0f5aea0d09efe3362af5
Karan Dhillon <Karanpal.dhillon@gmail.com>
2024-10-20 10:42:42 +0530

    surround

---
+++ lua/plugins/surround.lua
@@ -0,0 +1,20 @@
+return {
+  "kylechui/nvim-surround",
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup {
+      insert = "<C-g>i",
+      insert_line = "<C-g>S",
+      normal = "ys",
+      normal_cur = "yss",
+      normal_line = "yS",
+      normal_cur_line = "ySS",
+      visual = "S",
+      visual_line = "gS",
+      delete = "ds",
+      change = "cs",
+      change_line = "cS",
+    }
+  end,
+}
