b3ac77353b871aace429b9239db8dab46af66798
Karan Dhillon <Karanpal.dhillon@gmail.com>
2025-01-18 20:10:01 +0530

    copilot

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -5,7 +5,7 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
   -- { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.editing-support.treesj" },
---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,9 @@
+return {}
+-- return {
+--   "zbirenbaum/copilot.lua",
+--   cmd = "Copilot",
+--   opts = {
+--     suggestion = { enabled = false },
+--     panel = { enabled = false },
+--   },
+-- }
